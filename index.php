b27fe76c68c6e0792ca1c510bb06502a96d13b15
mktamim <tamimiqbal341060@gmail.com>
2022-01-05 22:45:58 +0600

    some file Updated

--- index.php
+++ index.php
@@ -10,8 +10,33 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inventory Managemant</title>
+    
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+    <link type="text/css" rel="stylesheet" href="css/style.css">
+
 </head>
 <body>
     
+    <div>
+        <h1 class="center-aline">Welcome to Your Inventory Boss</h1>
+    </div>
+
+    <div class="col-sm-6">
+        <a href="login.php">
+            <input type="button" class="btn btn-danger pull-right" value="Login"> 
+        </a>
+    </div>
+
+    <div class="col-sm-6">
+        <a href="register.php">
+            <input type="button" class="btn btn-success pull-left" value="Register"> 
+        </a>
+    </div>
+
 </body>
 </html>
