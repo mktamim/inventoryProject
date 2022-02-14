a7e6fc98a8c20339795595e291b23954b02dd2fc
mktamim <tamimiqbal341060@gmail.com>
2022-02-14 07:46:20 +0600

    commit

--- login.php
+++ login.php
@@ -1,16 +1,86 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-        <title>Login Form </title>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+<?php
 
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    include "auth/connection.php";
+    $conn= connect();
 
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    $m='';
+
+    if(isset($_POST['submit'])){
+        $uName= $_POST['uname'];
+        $pass= $_POST['pass'];
+
+        $sql= "SELECT * FROM user_info WHERE u_name='$uName' and password='$pass'";
+        $res= $conn->query($sql);
+
+        if(mysqli_num_rows($res)==1){
+            $user= mysqli_fetch_assoc($res);
+            header('Location: dashboard.php');
+        }
+        else{
+            $m= 'Cradiantials mismatch';
+        }
+    }
+
+?>
+
+
+<html>
+    <head>
         <link type="text/css" rel="stylesheet" href="css/style.css">
         <link type="text/css" rel="stylesheet" href="css/login.css">
-</head>
-<body>
-    
-</body>
+    </head>
+    <body>
+        <div class="logo">
+
+        </div>
+        <form method="POST">
+            <div class="box bg-img">
+                <div class="content">
+                        <h2>Log<span> In</span></h2>
+                    <div class="forms">
+                        <p style="color: red; padding: 20px;"><?php if($m!='') echo $m; ?></p>
+                        <div class="user-input">
+                            <input name="uname" type="text" class="login-input" placeholder="Username" id="name" required>
+                            <i class="fas fa-user"></i>
+                        </div>
+                        <div class="pass-input">
+                            <input name="pass" type="password" class="login-input" placeholder="Password" id="my-password" required>
+                            <span class="eye" onclick="myFunction()">
+                              <i id="hide-1" class="fas fa-eye-slash"></i>
+                              <i id="hide-2" class="fas fa-eye"></i>
+                            </span>
+                        </div>
+                    </div>
+
+                    <button class="login-btn" type="submit" name="submit">Sign In</button>
+                    <p class="new-account">Not a user? <a href="register.php">Sign Up now!</a></p>
+                    <br>
+
+                    <p class="f-pass">
+                        <a href="#">forgot password?</a>
+                    </p>
+
+                </div>
+            </div>
+        </form>
+        <script src="https://kit.fontawesome.com/c0078485ae.js" crossorigin="anonymous"></script>
+    </body>
 </html>
+<script>
+    function myFunction() {
+        var x = document.getElementById("my-password");
+        var y = document.getElementById("hide-1");
+        var z = document.getElementById("hide-2");
+
+        if (x.type === "password") {
+            x.type = "text";
+            y.style.display = "block";
+            z.style.display = "none";
+        } else {
+            x.type = "password";
+            y.style.display = "none";
+            z.style.display = "block";
+        }
+    }
+
+</script>
